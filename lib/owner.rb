647ca9a5ff65843299d655eea4e01a7afa93986f
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-30 20:51:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -53,6 +53,10 @@ class Owner
     pets[:cats].each { |cat| cat.mood = "happy"}
   end
 
+  def feed_fish
+    pet[:fishes].each { |fish| fish.mood = "happy"}
+  end
+
   #keeping going here
 
   def list_pets
